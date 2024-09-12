cc33e7d72625d7e8ddc362282868fae2ec1ac40f
carlosDani67 <136808028+carlosDani67@users.noreply.github.com>
2024-09-12 10:02:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document. querySelector(".caixa-perguntas");
+const caixaAlternativas = document. querySelector(".caixa-alternativas");
+const caixaResultado = document. querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+           "Alternativas 1"
+           "Alternativas 2"
+        ]
+        enunciado: "Pergunta 2",
+        alternativas: [
+           "Alternativas 1"
+           "Alternativas 2"
+           {
+            enunciado: "Pergunta 3",
+        alternativas: [
+           "Alternativas 1"
+           "Alternativas 2"
+
+           }
+{
+    enunciado: "Pergunta 4",
+        alternativas: [
+           "Alternativas 1"
+           "Alternativas 2"
+
+}
+{
+    enunciado: "Pergunta 5",
+        alternativas: [
+           "Alternativas 1"
+           "Alternativas 2"
+
+}
